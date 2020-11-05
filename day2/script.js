8b6cbe6500711835f5c571e156d73ba3a96eb136
Wendy C <chriscowendy@gmail.com>
2020-11-05 15:38:22 -0800

    added some more types and console logs

--- day2/script.js
+++ day2/script.js
@@ -1,2 +1,7 @@
 let largeWord = 'Supercalifragilisticexpialidocious';
 console.log(largeWord.length);
+console.log(10);
+console.log(typeof(10));
+let largeNum = BigInt(2);
+console.log(largeNum);
+console.log(typeof(largeNum));
