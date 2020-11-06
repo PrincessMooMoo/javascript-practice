d654c15232133af3c158dce374c34a3152b8cd1e
Wendy C <chriscowendy@gmail.com>
2020-11-05 16:19:55 -0800

    made new madLibs.js

---
+++ day2/madLibs.js
@@ -0,0 +1,52 @@
+// Assign STRING VALUES ONLY to all of the following variables.
+// Assign an adjective
+let adj1 = "soft";
+// Assign an adjective
+let adj2 = "hard";
+// Assign an adjective
+let adj3 = "fluffy";
+// Assign an adjective
+let adj4 = "rough";
+// Assign an adjective
+let adj5 = "squishy";
+// Assign an adjective
+let adj6 = "stinky";
+// Assign an adverb
+let adverb = "beautifully";
+// Assign a color
+let color = "orange";
+// Assign a noun
+let noun1 = "orangatang";
+// Assign a noun
+let noun2 = "cow";
+// Assign a noun
+let noun3 = "chimpanzee";
+// Assign a noun
+let noun4 = "radio";
+// Assign a noun
+let noun5 = "horse";
+// Assign a noun
+let noun6 = "tractor";
+// Assign a number
+let num = 10;
+// Assign a plural noun
+let pnoun1 = "geese";
+
+// Open your console to see the results!
+// SPOILER ALERT: KEEP SCROLLING TO VIEW THE REST OF THE CODE
+
+
+//Using template literals to plug in the values
+let story = `It has often been said that a dog is a man's best ${noun1}. Dogs are very ${adj1} and can be taught many ${adj2}
+tricks. A dog can be trained to carry a ${noun2} in his mouth.
+If you throw his ${noun3}, he will run and fetch it. Dogs
+will also bark ${adverb} if someone tries to break into your
+${noun4} during the night. One of the most popular canine pets
+today is the ${noun5} Spaniel. Spaniels have curly ${color}
+coats and ${adj3} ears. They also have very ${adj4}
+dispositions and live to be ${num} years old. Other popular dogs
+are ${adj5} Terriers, German ${pnoun1}, and the
+${adj6} Poodle. Every ${noun6} should have a loyal dog.`;
+
+//Console log the completed story
+console.log(story);
