a5bd7dd1b6ab23f10177e6111d38f3fbc9828fd3
Wendy C <chriscowendy@gmail.com>
2020-11-11 10:39:08 -0800

    worked on take home assignment

--- day3/day3TakeHomeChallenge.js
+++ day3/day3TakeHomeChallenge.js
@@ -7,3 +7,19 @@
 // Ask for the user's birthday
 // Ask the user to confirm their birthday input
 // Alert the user's birthday
+
+function userName() {
+    const firstName = prompt("What is your first name?", "John");
+    const lastName = prompt("What is your last name?", "Doe");
+    console.log(firstName);
+    alert(lastName);
+}
+
+function birthday() {
+    const birthDate = prompt("When is your birthday?", "01/01/2001");
+    let convertDate = new Date(birthDate);
+    confirm(`Is ${convertDate} your birthday, FO REALZ??`);
+    alert(convertDate);
+}
+userName();
+birthday();
