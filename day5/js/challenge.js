4b1aeebbaff913969c25a857bfa04010306f75b4
Wendy C <chriscowendy@gmail.com>
2020-11-16 20:04:16 -0800

    wrote notes in challenge.js

--- day5/js/challenge.js
+++ day5/js/challenge.js
@@ -1,7 +1,31 @@
-const name = 'global';
-function localFunction() {
-    const name = "local";
-    console.log(name);
+var pets = ['dog'];
+function petPets() {
+    this.pets.forEach( function (pet) {
+        console.log("i pet my", pet);
+    });
+}
+petPets();
+
+const user1 = {
+    pets: ['cat', 'spider'],
+    petPets: petPets,
+};
+const user2 = {
+    pets: ['dog', 'fish'],
+    petPets: petPets,
+};
+function localFunction(){
+    const name = 'local';
 }
 localFunction();
-console.log(name);
+console.log(user2.petPets());
+
+
+// const name = 'global';
+// function localFunction() {
+//     const name = "local";
+//     console.log(name);
+// }
+// localFunction();
+// console.log(name);
+// console.log(user1.pets);
