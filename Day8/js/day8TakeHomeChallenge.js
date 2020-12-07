2c43af5d049e3fbe5b4af23a6a837e79073ca0e6
Wendy C <chriscowendy@gmail.com>
2020-12-07 09:19:23 -0800

    completed daily take home challenge

--- Day8/js/day8TakeHomeChallenge.js
+++ Day8/js/day8TakeHomeChallenge.js
@@ -12,6 +12,9 @@ class Car {
         this.year = year;
         this.color = color;
     }
+    toString(){
+        return `${this.name} ${this.model} ${this.year} ${this.color}`;
+    }
 }
 
 let myCar1 = new Car('Ford', 'F-150', 2000, 'Green');
@@ -19,3 +22,15 @@ let myCar2 = new Car('Honda', 'Civic', 2011, 'Charcoal Gray');
 let myCar3 = new Car('MG', 'B', 1963, 'White');
 let myCar4 = new Car('Nissan', 'Ultima', 2019, 'Red');
 console.log(myCar1, myCar2, myCar3, myCar4);
+
+const liMyCar1 = document.querySelector('#class1');
+liMyCar1.innerHTML = myCar1.toString();
+
+const liMyCar2 = document.querySelector('#class2');
+liMyCar2.innerHTML = myCar2.toString();
+
+const liMyCar3 = document.querySelector('#class3');
+liMyCar3.innerHTML = myCar3.toString();
+
+const liMyCar4 = document.querySelector('#class4');
+liMyCar4.innerHTML = myCar4.toString();
